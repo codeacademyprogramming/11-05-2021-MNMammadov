67e8c50a03ce3f0d44fc44de3fe6c72849d4ed07
MNMammadov <mehdinm@code.edu.az>
2021-05-12 01:02:25 +0400

    Added loading animation

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,9 @@ import { renderCustomerTable } from './customerTable.js';
 async function initApp() {
     await fetchUserData();
     const customers = await fetchCustomers();
+    const loadingAnimation = document.getElementById('loading-animation');
+    loadingAnimation.classList.add("hidden");
+
     initHeader(customers);
     renderCustomerTable(customers);
 }
